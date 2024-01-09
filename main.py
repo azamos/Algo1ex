9703ec1518598fd17a171edd214733d3d92fb6cd
amos zohar <azamoszohar@gmail.com>
2024-01-09 16:22:51 +0200

    fixed input issue

--- main.py
+++ main.py
@@ -9,20 +9,19 @@ for i in range(1,G.n):#Adding succesive jumps of 1: 19 edges
     #(13,14),(14,15),(15,16),(16,17),(17,18),(18,19),(19,20)
     G.addEdge(i,i+1,1)
 
-for i in range(2,G.n-1,2):#adding succesive jumps of 2: 18 edges
+for i in range(1,G.n-1):#adding succesive jumps of 2: 18 edges
+    #(1,3),(3,5),(5,7),(7,9),(9,11),(11,13),(13,15),(15,17),(17,19)
     #(2,4),(4,6),(6,8),(8,10),(10,12),(12,14),(14,16),(16,18),(18,20)
     G.addEdge(i,i+2,2)
-    #(1,3),(3,5),(5,7),(7,9),(9,11),(11,13),(13,15),(15,17),(17,19)
-    G.addEdge(i-1,i+1,2)
 
-for i in range(3,G.n-2,3):#Adding succesive jumps of 3 = 17 edges 
+for i in range(1,G.n-2):#Adding succesive jumps of 3 = 17 edges
+    #(1,4),(4,7),(7,10),(10,13),(13,16),(16,19) 
+    #(2,5),(5,8),(8,11),(11,14),(14,17),(17,20)
     #(3,6),(6,9),(9,12),(12,15),(15,18)
     G.addEdge(i,i+3,3)
-    #(2,5),(5,8),(8,11),(11,14),(14,17),(17,20)
-    G.addEdge(i-1,i+2,3)
-    #(1,4),(4,7),(7,10),(10,13),(13,16),(16,19)
-
+    
 #Now I have a total of 54 edges(
+print(f"|edges| = {len(G.edges.items())}")
 print("G is: ")
 G.printGraph()
 print("_________________________")
